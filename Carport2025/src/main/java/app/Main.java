bc6a6db80f784ca2f1c023368d41cbb53d2e21a3
niki03125 <Nikita.po@hotmail.com>
2025-05-05 10:58:39 +0200

    adding UserMapper.java

--- Carport2025/src/main/java/app/Main.java
+++ Carport2025/src/main/java/app/Main.java
@@ -2,7 +2,6 @@ package app;
 
 import app.config.SessionConfig;
 import app.config.ThymeleafConfig;
-import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinThymeleaf;
 
