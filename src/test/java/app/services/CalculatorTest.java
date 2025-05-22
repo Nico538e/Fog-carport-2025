45d876141a0b2b1f5074e4f66c21084f628bb894
niki03125 <Nikita.po@hotmail.com>
2025-05-22 13:14:51 +0200

    reset

---
+++ src/test/java/app/services/CalculatorTest.java
@@ -0,0 +1,30 @@
+package app.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void calculate() {
+    }
+
+    @Test
+    void calcPosts() {
+    }
+
+    @Test
+    void calcPostQuantity() {
+    }
+
+    @Test
+    void calcBeams() {
+    }
+
+    @Test
+    void calcRafters() {
+    }
+}
