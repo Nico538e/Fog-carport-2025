96992a561ca294a2ae34c27ebdd64283b27138ba
niki03125 <Nikita.po@hotmail.com>
2025-05-23 14:35:33 +0200

    deployment

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -30,7 +30,7 @@ public class Main {
             config.staticFiles.add("/public");
             config.jetty.modifyServletContextHandler(handler ->  handler.setSessionHandler(SessionConfig.sessionConfig()));
             config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
-        }).start(7070);
+        }).start(7071);
 
         // Routing
 
